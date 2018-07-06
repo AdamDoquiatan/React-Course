33fbcbc77c8b7524c572abbe4202197189a6dfb8
AdamDoquiatan <adamdoq@gmail.com>
2018-07-05 20:35:54 -0700

    Lecture 29

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -27,19 +27,28 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick')
+  }
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     )
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('REMOVING ALL')
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>
+          Remove All
+        </button>
         {
           this.props.options.map((option) => <Option key={option} optionText={option} />)
         }
@@ -61,10 +70,23 @@ class Option extends React.Component {
 
 
 class AddOption extends React.Component {
+  
+  handleAddOption(e) {
+    e.preventDefault()
+
+    const option = e.target.elements.option.value.trim()
+
+    if (option) {
+      alert(option)
+    }
+  }
   render() {
     return (
       <div>
-        This is the Add Option component
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
+        </form>
       </div>
     )
   }
