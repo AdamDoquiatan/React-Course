655d0e0f9ed25c174f8742bacdbf333314af1753
Adam Doquiatan <adamdoq@gmail.com>
2018-07-09 17:40:25 -0700

    Lecture 47

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -6,8 +6,31 @@ class IndecisionApp extends React.Component {
     this.handleAddOption = this.handleAddOption.bind(this)
     this.handleDeleteOption = this.handleDeleteOption.bind(this)
     this.state = {
-      options: props.options
+      options: []
     }
+  }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options')
+      const options = JSON.parse(json)
+
+      if (options) {
+        this.setState(() => ({ options: options }))
+      }
+    } catch (e) {
+
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options)
+      localStorage.setItem('options', json)
+      console.log('saving data')
+    }
+
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
   }
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }))
@@ -53,10 +76,6 @@ class IndecisionApp extends React.Component {
   }
 }
 
-IndecisionApp.defaultProps = {
-  options: []
-}
-
 const Header = (props) => {
   return (
     <div>
@@ -87,6 +106,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}> Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started</p>}
       {
         props.options.map((option) => (
           <Option
@@ -127,9 +147,14 @@ class AddOption extends React.Component {
 
     const option = e.target.elements.option.value.trim()
     const error = this.props.handleAddOption(option)
-    e.target.elements.option.value = ''
+    
 
-    this.setState(() => ({ error }))}
+    this.setState(() => ({ error }))
+
+    if (!error) {
+      e.target.elements.option.value = ''
+    }
+  }
   render() {
     return (
       <div>
--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,9 +5,24 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this)
     this.handleReset = this.handleReset.bind(this)
     this.state = {
-      count: props.count
+      count: 0
     }
   }
+  componentDidMount(prevState, prevProps){
+    const stringCount = localStorage.getItem('count')
+    const count = parseInt(stringCount, 10)
+
+    if (!isNaN(count)) {
+      this.setState((prevState) => ({ count: count }))
+    }
+  }
+
+  componentDidUpdate(prevState, prevProps) {
+    if (prevState.count != this.state.count ) {
+      localStorage.setItem('count', this.state.count)
+    }
+  }
+
   handleAddOne() {
     this.setState((prevState) => {
       return {
@@ -42,41 +57,4 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-  count: 0
-}
-
 ReactDOM.render(<Counter count={4}/>, document.querySelector('#app'))
-
-// let count = 0
-// const addOne = () => {
-//     count++
-//     renderCounterApp()
-// }
-
-// const minusOne = () => {
-//     count--
-//     renderCounterApp()
-// }
-
-// const reset = () => {
-//     count = 0
-//     renderCounterApp()
-// }
-
-// const appRoot = document.getElementById('app')
-
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={addOne}>+1</button>
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={reset}>reset</button>
-//         </div>
-//     )
-
-//     ReactDOM.render(templateTwo, appRoot)
-// }
-
-// renderCounterApp()
